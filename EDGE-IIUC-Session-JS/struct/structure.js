30b1aadb9501a687d6f071cb8b48e61185785091
farzana-yeasmin-etuu <137638172+farzana-yeasmin-etuu@users.noreply.github.com>
2024-11-08 17:44:26 +0600

    Add files via upload

---
+++ EDGE-IIUC-Session-JS/struct/structure.js
@@ -0,0 +1,17 @@
+//alert(3 +
+  //  1
+  //  + 2);
+
+
+  //alert("Hello");
+
+//[1, 2].forEach(alert);
+
+
+let name = "Etuu";
+
+// embed a variable
+alert( `Hello, ${name}!` ); // Hello, John!
+
+// embed an expression
+alert( `the result is ${1 + 2}` ); // the result is 3
